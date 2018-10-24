1a650c1451c6138e24d5a234a86759dfcf546bbd
yanyumiao <357596508@qq.com>
2018-10-24 20:46:57 +0800

    add example.go

---
+++ example.go
@@ -0,0 +1,14 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run(":9000") // listen and serve on 0.0.0.0:8080
+	// router.Run(":3000") for a hard coded port
+}
