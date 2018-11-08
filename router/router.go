2640ff9675d8f0c9640516612930563118018b8e
yanyumiao <357596508@qq.com>
2018-11-08 22:49:10 +0800

    add resource/favicon.ico

--- router/router.go
+++ router/router.go
@@ -8,6 +8,7 @@ func InitRouter() *gin.Engine {
 	// router
 	router.GET("/", Hello)
 	router.GET("/test", Test)
+	router.StaticFile("/favicon.ico", "./resource/favicon.ico") // 相对路径 相对main.go
 	//
 	return router
 }
