58b3084b767f205c47a95860d4ef74adc5f1367e
yanyumiao <357596508@qq.com>
2018-11-19 23:36:58 +0800

    强制类型转换 注释++

--- controller/test.go
+++ controller/test.go
@@ -22,6 +22,7 @@ func GetPerson(c *gin.Context) {
 	var id = c.Param("id") // Notice: string
 	// Notice: 强制类型转换
 	// TODO 总结Go的类型转换
+	// 参考:http://blog.sina.com.cn/s/blog_537517170102xkc2.html
 	p.Id, _ = strconv.Atoi(id)
 	person := p.Get()
 	c.String(http.StatusOK, person.FirstName)
