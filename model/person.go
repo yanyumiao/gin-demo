da9d6cf4dde2ccf454a1d47d4396171281700c77
yanyumiao <357596508@qq.com>
2018-11-24 18:53:47 +0800

    model/person.go +

--- model/person.go
+++ model/person.go
@@ -11,22 +11,21 @@ type Person struct {
 	LastName  string `json:"last_name" form:"last_name"`
 }
 
-func (p *Person) AddPerson() (id int64, err error) {
-	rs, err := db.SqlDB.Exec("INSERT INTO person(first_name, last_name) VALUES (?, ?)", p.FirstName, p.LastName)
-	if err != nil {
-		return
-	}
-	id, err = rs.LastInsertId()
-	return
-}
-
 // author: YanYuMiao
 func (p *Person) Get() (person Person) {
 	// TODO 翻译总结 go-database-sql.org
 	// http://go-database-sql.org/retrieving.html
 	row := db.SqlDB.QueryRow("SELECT id, first_name, last_name FROM person WHERE id = ?", p.Id)
 	row.Scan(&person.Id, &person.FirstName, &person.LastName)
-	// TODO
+	return
+}
+
+func (p *Person) AddPerson() (id int64, err error) {
+	rs, err := db.SqlDB.Exec("INSERT INTO person(first_name, last_name) VALUES (?, ?)", p.FirstName, p.LastName)
+	if err != nil {
+		return
+	}
+	id, err = rs.LastInsertId()
 	return
 }
 
