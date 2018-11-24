9259ad90f9f54fa9ebacc6781acf48136eeded48
yanyumiao <357596508@qq.com>
2018-11-24 18:04:22 +0800

    controller: del test.go & add person.go

--- controller/person.go
+++ controller/person.go
@@ -5,18 +5,6 @@ import "net/http"
 import model "gin/model"
 import "strconv"
 
-func Test(c *gin.Context) {
-	c.String(http.StatusOK, "test")
-}
-
-func Pong(c *gin.Context) {
-	// type H map[string]interface{}
-	// H is a shortcut for map[string]interface{}
-	// interface{}接口类型 表示value可以是任何数据类型
-	c.JSON(200, map[string]interface{}{"code": 1, "msg": "pong"})
-	//c.JSON(200, gin.H{"code": 1, "msg": "pong"})
-}
-
 func GetPerson(c *gin.Context) {
 	var p model.Person
 	var id = c.Param("id") // Notice: string
