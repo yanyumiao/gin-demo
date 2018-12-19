ad7d35f7d0cb431f4d82fb4d54a5464486c2997c
yanyumiao <357596508@qq.com>
2018-12-19 22:49:00 +0800

    README++

--- README.md
+++ README.md
@@ -2,5 +2,5 @@ gin-demo
 A demo about use gin to develop web application  
 
 1 setup  
-go get -u github.com/gin-gonic/gin  
-go get -u github.com/go-sql-driver/mysql  
+go get github.com/gin-gonic/gin  
+go get github.com/go-sql-driver/mysql  
--- router/router.go
+++ router/router.go
@@ -16,6 +16,6 @@ func InitRouter() *gin.Engine {
 	router.GET("/getallperson", GetAllPerson)
 	router.GET("/delperson", DelPerson)       // ?id=4
 	router.GET("/updateperson", UpdatePerson) // ?id=4&firstname=a&lastname=b
-	//router.StaticFile("/favicon.ico", "./resource/favicon.ico") // 相对路径 相对main.go
+	//router.StaticFile("/favicon.ico", "./resource/favicon.ico")
 	return router
 }
