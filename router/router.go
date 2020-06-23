1c53488450b6e77cd1e56ff52ee70983aa3f2e2b
YanYuMiao <357596508@qq.com>
2020-06-24 00:39:05 +0800

    note --

--- router/router.go
+++ router/router.go
@@ -1,7 +1,7 @@
 package router
 
 import "github.com/gin-gonic/gin"
-import . "gin-demo/controller" // Notice: ".", 点操作 导入包后 调用这个包的函数时 可以省略包名
+import . "gin-demo/controller"
 
 func InitRouter() *gin.Engine {
 	router := gin.Default()
@@ -16,8 +16,8 @@ func InitRouter() *gin.Engine {
 	router.GET("/person/add", AddPerson) // http://127.0.0.1:10086/person/add?firstname=a&lastname=b
 	router.GET("/person/get", GetPerson) // http://127.0.0.1:10086/person/get?id=1
 	router.GET("/person/all", GetAllPerson)
-	router.GET("/person/del", DelPerson)       // ?id=4
-	router.GET("/person/update", UpdatePerson) // ?id=4&firstname=a&lastname=b
+	router.GET("/person/del", DelPerson)       // http://127.0.0.1:10086/person/del?id=4
+	router.GET("/person/update", UpdatePerson) // http://127.0.0.1:10086/person/update?id=4&firstname=a&lastname=b
 	// router.StaticFile("/favicon.ico", "./resource/favicon.ico")
 	return router
 }
